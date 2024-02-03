d12b1b1894f2a062e7ac0195df7fd0a61e95819c
Adricarmona <adricarmonagalvez@gmail.com>
2024-02-03 12:49:51 +0100

    lo de los pokemons

--- javascript_index.js
+++ javascript_index.js
@@ -1,19 +1,32 @@
-let pokemonsTotales = [];
+
+////////////////// devuelve el pokemon en el array pokemonsTotaless (completado) //////////
+const pokemonsTotales = [];
 
 for (let index = 1; index < 151; index++) {
-    damePokemonYa(index)
-
+    //damePokemonYa(index)
+    devolverArray(index)
 }
 
-// devuelve el pokemon en el array pokemonsTotaless
-async function devolverArray(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemonJson = await response.text();
-    const obj = JSON.parse(pokemonJson);
-    pokemonsTotales[id] = obj;
-    console.log(pokemonsTotales[id]);
+async function devolverArray(id) { // Coge la id del pokemon dado en el bucle encima
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`); // coge la informacion de la ap y la mete en response
+    const pokemonJson = await response.text(); // la transforma en texto 
+    const obj = JSON.parse(pokemonJson); // La transforma de json a objeto 
+    pokemonsTotales[id] = obj; // lo pasa a un array donde estan todos los pokemons
+    //console.log(pokemonsTotales[id]); // lo pasa a consola
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+console.log(pokemonsTotales);
+
+
+
+
+
+
+
+
+/*
 async function damePokemonYa(id) {
     console.log(123);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -24,6 +37,7 @@ async function damePokemonYa(id) {
     document.getElementById('name').innerText = obj.name;
     document.getElementById('imagen').src = obj.sprites.front_default;
 }
+*/
 
 // CODIGO PARA MODO OSCURO-CLARO
 //*
@@ -37,6 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const body = document.body;
     const modoOscuroBtn = document.querySelector(".modoOscuro");
     modoOscuroBtn.addEventListener("click", toggleModoOscuro);
+
     function toggleModoOscuro() {
         if (body.classList.contains("modo-oscuro")) {
             body.classList.remove("modo-oscuro");
@@ -46,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
             fadeBackground("./imagenes/fondo_oscuro_pokeapi.jpg");
         }
     }
+
     function fadeBackground(nuevaImagen) {
         const fondoActual = body.style.backgroundImage;
         const nuevaFondo = `url('${nuevaImagen}')`;
@@ -62,4 +78,5 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         }, transicionDuracion / (1 / incrementoOpacidad));
     }
+
 });
