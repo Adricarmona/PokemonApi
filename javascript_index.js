56b778a92957eac3235557c2159af80919cffb86
Adricarmona <adricarmonagalvez@gmail.com>
2024-02-03 15:24:08 +0100

    mas mierda para imprimir los pokemons

--- javascript_index.js
+++ javascript_index.js
@@ -1,44 +1,44 @@
-
 ////////////////// devuelve el pokemon en el array pokemonsTotales (completado) //////////
 const pokemonsTotales = [];
 
-for (let index = 1; index < 151; index++) {
-    //damePokemonYa(index)
-    devolverArray(index)
+async function cargarPokemons() {
+    for (let index = 1; index < 151; index++) {
+        //damePokemonYa(index)
+        await devolverArray(index);
+    }
+
+    // Imprime los nombres de todos los pokemons
+    pokemonsTotales.forEach(pokemon => console.log(pokemon.name));
 }
+
+console.log(pokemonsTotales);
 
 async function devolverArray(id) { // Coge la id del pokemon dado en el bucle encima
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`); // coge la informacion de la ap y la mete en response
     const pokemonJson = await response.text(); // la transforma en texto 
     const obj = JSON.parse(pokemonJson); // La transforma de json a objeto 
     pokemonsTotales[id] = obj; // lo pasa a un array donde estan todos los pokemons
-    //console.log(pokemonsTotales[id]); // lo pasa a consola
 }
 
-console.log(pokemonsTotales);
+cargarPokemons();
+
 //////////////////////////////////////////////////////////////////////////////////////////
+
 
 //////////// LO DE IR GENERANDO TEXTOS Y ESO /////////////////////////////////////////////
+const listaPokemon = document.querySelector("#base");   
 
 function imprimirPokemons(pokemonsTotales) {
-    for (let index = 1; index <= pokemonsTotales; index++) {
-        const h3 = document.createElement('h3');
-        const img = document.createElement('img');
-        h3.textContent = 'test'; // Assign text to h3 element
-        
-        let pokemons = document.getElementById('base');
-        pokemons.appendChild(h3); // Append h3 to the 'base' element
-    }
+    const div = document.createElement('div');
+    div.classList.add('pokemons');
+    div.innerHTML = `
+    <img id="imagen" width="200px" src="./imagenes/pokemon_trabajando.jpg">
+    <h2 id="name">${pokemon.name}</h2>
+    <p id="codigo">codigo</p>`;
+    listaPokemon.append(div);
 }
 
-imprimirPokemons(151); // Call the function with the total number of Pokémon
-
-
 //////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
 
 /*
 async function damePokemonYa(id) {
