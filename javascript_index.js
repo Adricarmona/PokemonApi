c5a789bb22b2097820d6f6b777dc33f8aabb05bf
Adricarmona <adricarmonagalvez@gmail.com>
2024-02-08 01:45:49 +0100

    arreglando todo lo de aparecer pokemons
    Esto creo que en algunos paises se llama magia

--- javascript_index.js
+++ javascript_index.js
@@ -1,47 +1,48 @@
-////////////////// devuelve el pokemon en el array pokemonsTotales (completado) //////////
-const pokemonsTotales = [];
+cargarPokemons(); // para iniciar la busqueda
 
+////////////////// devuelve el pokemon en el array pokemonsTotales ///////////////////////
+const pokemonsTotales = []; // array donde esta todos los pokemons
+/// esta funcion coge todos los pokemons
 async function cargarPokemons() {
+    // coge los 151 y los mete en el array
     for (let index = 1; index < 151; index++) {
-        //damePokemonYa(index)
         await devolverArray(index);
     }
-
-    // Imprime los nombres de todos los pokemons (Transformar en for o asi)
-    pokemonsTotales.forEach(pokemon => {if (pokemon.type.length > 1) {
-        console.log(pokemon.name, pokemon.types[0].type.name, pokemon.types[1].type.name)
-    } else {
-        console.log(pokemon.name, pokemon.types[0].type.name)
-    }} );
+    // coge los poquemos uno a uno y mete el que este utilizando en pokemon y lo mete en al funcion imprimir pokemons
+    // asi hasta que se termine el forEach
+    pokemonsTotales.forEach(pokemon => imprimirPokemons(pokemon));
 }
+//////////////////////////////////////////////////////////////////////////////////////////
 
-console.log(pokemonsTotales);
 
+///////////////////////// esta es la funcion que devuelve el Json a el text array ////////////////////////////
 async function devolverArray(id) { // Coge la id del pokemon dado en el bucle encima
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`); // coge la informacion de la ap y la mete en response
     const pokemonJson = await response.text(); // la transforma en texto 
     const obj = JSON.parse(pokemonJson); // La transforma de json a objeto 
     pokemonsTotales[id] = obj; // lo pasa a un array donde estan todos los pokemons
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-cargarPokemons();
+console.log(pokemonsTotales)
 
-//////////////////////////////////////////////////////////////////////////////////////////
-
-
-//////////// LO DE IR GENERANDO TEXTOS Y ESO /////////////////////////////////////////////
-const listaPokemon = document.querySelector("#base");   
-
-function imprimirPokemons(pokemonsTotales) {
-    const div = document.createElement('div');
-    div.classList.add('pokemons');
-    div.innerHTML = `
-    <img id="imagen" width="200px" src="./imagenes/pokemon_trabajando.jpg">
-    <h2 id="name">${pokemon.name}</h2>
-    <p id="codigo">codigo</p>`;
-    listaPokemon.append(div);
+//////////// LO DE IR GENERANDO LOS POKEMONS CON LAS FOTOS ///////////////////////////////
+/////////ESTE ES COMO ESTA PENSADO EL DIV pokemon POR AHORA ////////////////
+/*
+<div class="pokemons">
+    <a href="./vista_pokemon/vista_pokemon.html">
+        <img id="imagen" width="200px" src="./imagenes/pokemon_trabajando.jpg">
+        <h2 id="name">nombre</h2>
+        <p id="codigo">codigo</p>
+    </a>
+</div>
+*/
+function imprimirPokemons(pokemon) {
+    var zonaPokemon = document.createElement('div');
+    zonaPokemon.classList.add('pokemons');
+    zonaPokemon.innerHTML = `<a id="linkPokemon" href="./vista_pokemon/vista_pokemon.html"><img id="imagen" width="200px" src="${pokemon.sprites.other.home.front_default}"><h2 id="name">${pokemon.name}</h2><p id="codigo">ID: ${pokemon.id}</p></a>`;
+    document.getElementById('Base').appendChild(zonaPokemon);
 }
-
 //////////////////////////////////////////////////////////////////////////////////////////
 
 /*
