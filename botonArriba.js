e08fb64d937443a2cb9ce0fd2d42542b8c066413
Adricarmona <adricarmonagalvez@gmail.com>
2024-02-10 14:23:16 +0100

    boton de arriba ya completado

--- botonArriba.js
+++ botonArriba.js
@@ -1,8 +1,7 @@
-
+    // esto es solo para el boton de subir para arriba 
     const botonjs = document.querySelector("#botonPaArriba");
     botonjs.addEventListener("click", subirArriba);
 
     function subirArriba() {
-        console.log("si");
-        window.onscroll
+        scrollTo(0,0) // mueve a la pagina a la 0,0 osea arriba a la izquierda
     }
