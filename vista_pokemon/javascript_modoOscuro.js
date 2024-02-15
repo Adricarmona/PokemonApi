cf82d2ee730b4b033e15f019136eeede49ec38bd
Rikitr <rtroost548@g.educaand.es>
2024-02-15 17:18:31 +0100

    cambios en la pagina de vista_pokemon
    boton modo oscuro claro y ajustes

---
+++ vista_pokemon/javascript_modoOscuro.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.body;
+    const modoOscuroBtn = document.querySelector("#modoOscuro");
+    modoOscuroBtn.addEventListener("click", toggleModoOscuro);
+    function toggleModoOscuro() {
+        modoOscuro = !modoOscuro; // Cambia el estado entre true y false
+        if (modoOscuro) {
+            body.classList.add("modo-oscuro");
+            fadeBackground("../imagenes/fondo_oscuro_pokeapi.jpg");
+        } else {
+            body.classList.remove("modo-oscuro");
+            fadeBackground("../imagenes/fondo_claro_pokeapi.jpg");
+        }
+    }
+
+    function fadeBackground(nuevaImagen) {
+        const fondoActual = body.style.backgroundImage;
+        const nuevaFondo = `url('${nuevaImagen}')`;
+        const transicionDuracion = 500; 
+        const incrementoOpacidad = 0.02;
+        let opacidad = 1;
+        const intervalo = setInterval(function () {
+            opacidad -= incrementoOpacidad;  
+            if (opacidad <= 0) {
+                body.style.backgroundImage = nuevaFondo;
+                clearInterval(intervalo);
+            } else {
+                body.style.backgroundImage = `linear-gradient(rgba(255, 255, 255, ${opacidad}), rgba(255, 255, 255, ${opacidad})), ${fondoActual}`;
+            }
+        }, transicionDuracion / (1 / incrementoOpacidad));
+    }
+});
