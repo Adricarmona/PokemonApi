22356fc22012f3df4e575620b85e58cfe9fda7f8
Rikitr <rtroost548@g.educaand.es>
2024-02-22 16:26:59 +0100

    Update javascript_modoOscuro.js

--- vista_pokemon/javascript_modoOscuro.js
+++ vista_pokemon/javascript_modoOscuro.js
@@ -1,31 +1,31 @@
+// MODO CLARO Y MODO OSCURO
+
 document.addEventListener("DOMContentLoaded", function () {
     const body = document.body;
     const modoOscuroBtn = document.querySelector("#modoOscuro");
     modoOscuroBtn.addEventListener("click", toggleModoOscuro);
+
     function toggleModoOscuro() {
-        modoOscuro = !modoOscuro; // Cambia el estado entre true y false
-        if (modoOscuro) {
-            body.classList.add("modo-oscuro");
-            fadeBackground("../imagenes/fondo_oscuro_pokeapi.jpg");
-        } else {
+        if (body.classList.contains("modo-oscuro")) {
             body.classList.remove("modo-oscuro");
             fadeBackground("../imagenes/fondo_claro_pokeapi.jpg");
+        } else {
+            body.classList.add("modo-oscuro");
+            fadeBackground("../imagenes/fondo_oscuro_pokeapi.jpg");
         }
     }
 
     function fadeBackground(nuevaImagen) {
-        const fondoActual = body.style.backgroundImage;
-        const nuevaFondo = `url('${nuevaImagen}')`;
         const transicionDuracion = 500; 
         const incrementoOpacidad = 0.02;
-        let opacidad = 0;
+        let opacidad = 1;
         const intervalo = setInterval(function () {
-            opacidad -= incrementoOpacidad;  
+            opacidad -= incrementoOpacidad;
             if (opacidad <= 0) {
-                body.style.backgroundImage = nuevaFondo;
                 clearInterval(intervalo);
+                body.style.backgroundImage = `url('${nuevaImagen}')`;
             } else {
-                body.style.backgroundImage = `linear-gradient(rgba(255, 255, 255, ${opacidad}), rgba(255, 255, 255, ${opacidad})), ${fondoActual}`;
+                body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, ${opacidad}), rgba(0, 0, 0, ${opacidad})), url('${nuevaImagen}')`;
             }
         }, transicionDuracion / (1 / incrementoOpacidad));
     }
